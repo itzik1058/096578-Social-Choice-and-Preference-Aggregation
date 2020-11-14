a23864673b1f4943ea3261218f851a0c65977cca
Itzik <74200683+itzik1058@users.noreply.github.com>
2020-11-14 12:42:41 +0200

    Update bader_ofer.py
    Implement bader ofer method

--- 1/src/bader_ofer.py
+++ 1/src/bader_ofer.py
@@ -30,7 +30,31 @@ def bader_ofer(election, data, odafim_pairs):
 
     return - a dictionary mapping every party in data to the mandates it should receive.
     """
-    return "IMPLEMENT ME"
+    num_votes = sum(data.values())
+    mandate_cost = num_votes / PARLIAMENT_SIZE
+    party_votes = {p: v for p, v in data.items() if v > num_votes * threshold(election)}
+    party_mandates = {p: int(v / mandate_cost) for p, v in party_votes.items()}
+    leftover_mandates = PARLIAMENT_SIZE - sum(party_mandates.values())
+    grouped_votes = {}
+    grouped_mandates = {}
+    for p in party_votes:
+        if p in odafim_pairs:
+            pair_id, pair_party = odafim_pairs[p]
+            grouped_votes[pair_id] = party_votes[p] + party_votes[pair_party]
+            grouped_mandates[pair_id] = party_mandates[p] + party_mandates[pair_party]
+        else:
+            grouped_votes[p] = party_votes[p]
+            grouped_mandates[p] = party_mandates[p]
+    for i in range(leftover_mandates):
+        grouped_worth = {p: v / (grouped_mandates[p] + 1) for p, v in grouped_votes.items()}
+        winner = max(grouped_worth, key=grouped_worth.get)
+        party_winner = winner
+        if winner in odafim_pairs:
+            party_worth = {p: party_votes[p] / (party_mandates[p] + 1) for p in odafim_pairs[winner]}
+            party_winner = max(odafim_pairs[winner], key=party_worth.get)
+        grouped_mandates[winner] += 1
+        party_mandates[party_winner] += 1
+    return party_mandates
 
 
 elections = {}
