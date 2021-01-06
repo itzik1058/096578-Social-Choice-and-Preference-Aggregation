4376d4beeee890fdcede48af3d5845dc8cdfedd3
itzik1058 <itzik1058@gmail.com>
2021-01-06 10:16:44 +0200

    Update hw2.py
    Add proxy truth discovery with log odds weights

--- 2/hw2.py
+++ 2/hw2.py
@@ -47,6 +47,17 @@ def unweighted(votes, rule):
     return rule(votes), None
 
 
+def grofman_truth_discovery(votes, rule):
+    n_voters = votes.shape[0]
+    distance = np.empty(shape=(n_voters, n_voters))
+    for v1, v2 in product(range(n_voters), repeat=2):
+        distance[v1, v2] = kendall_tau_distance(votes[v1], votes[v2])
+    pi = (np.sum(distance, axis=1) - np.diag(distance)) / (n_voters - 1)
+    pi = pi * (1 - 1e-8) + 1e-8  # Make sure pi is not 0 or 1
+    weights = np.log(1 - pi) - np.log(pi)
+    return rule(votes, weights)
+
+
 def weighted_borda(votes, weights=None):
     n_voters, n_candidates = votes.shape
     if weights is None:
@@ -84,29 +95,42 @@ def plot_proxy_truth_distance(votes, true_ranking):
 
 
 def plot_average_error(votes, true_ranking, n_iter=500):
-    for p, rule in enumerate([weighted_borda, weighted_copeland]):
+    plt.figure(figsize=(10, 7))
+    for p, (rule, name) in enumerate([(weighted_borda, 'Borda'), (weighted_copeland, 'Copeland')]):
         plt.subplot(2, 1, p + 1)
         voters = np.arange(85)
         sample_size = np.arange(5, 90, 5)
-        average_true_error = np.empty(shape=(3, sample_size.shape[0]))
+        average_true_error = np.empty(shape=(4, sample_size.shape[0]))
         for i, size in enumerate(sample_size):
+            print(f'{rule.__name__} with sample size {size}')
             for _ in range(n_iter):
                 sample_votes = votes[np.random.choice(voters, size)]
                 for m, method in enumerate([proxy_truth_discovery, distance_truth_discovery, unweighted]):
                     average_true_error[m, i] += kendall_tau_distance(true_ranking, method(sample_votes, rule)[0])
+                grofman_truth = grofman_truth_discovery(sample_votes, weighted_borda)
+                average_true_error[3, i] += kendall_tau_distance(true_ranking, grofman_truth)
             average_true_error[:, i] /= n_iter
         plt.plot(sample_size, average_true_error[0], label='Proxy Truth Discovery')
         plt.plot(sample_size, average_true_error[1], label='Distance Truth Discovery')
         plt.plot(sample_size, average_true_error[2], label='Unweighted')
+        plt.plot(sample_size, average_true_error[3], label='Grofman Truth Discovery with Borda')
         plt.xlabel('Sample size')
-        plt.title('Average true distance')
+        plt.ylabel('Average true distance')
+        plt.title(name)
         plt.legend()
+    plt.subplots_adjust()
     plt.show()
 
 
 def main():
     votes = pd.read_csv('voters.csv', header=None).to_numpy(dtype=np.int)
     truth = pd.read_csv('truth.csv', header=None).to_numpy(dtype=np.int)
+    results = []
+    for method in (proxy_truth_discovery, distance_truth_discovery, unweighted):
+        for rule in (weighted_borda, weighted_copeland):
+            results.append(method(votes, rule)[0])
+    results.append(grofman_truth_discovery(votes, weighted_borda))
+    pd.DataFrame(results).to_csv('estimations.csv', header=False, index=False)
     truth_candidates, truth_ranking = truth
     truth_ranking = truth_candidates[truth_ranking]
     # Keep only relevant votes
@@ -114,13 +138,9 @@ def main():
     # Map remaining candidates into a smaller range
     truth_ranking_mapped = np.digitize(truth_ranking.ravel(), np.sort(truth_ranking), right=True)
     truth_votes_mapped = np.digitize(truth_votes.ravel(), np.sort(truth_ranking), right=True).reshape(truth_votes.shape)
+    # Plots:
     # plot_proxy_truth_distance(truth_votes_mapped, truth_ranking_mapped)
-    plot_average_error(truth_votes_mapped, truth_ranking_mapped)
-    results = []
-    for method in (proxy_truth_discovery, distance_truth_discovery, unweighted):
-        for rule in (weighted_borda, weighted_copeland):
-            results.append(method(votes, rule))
-    pd.DataFrame(results).to_csv('estimations.csv')
+    # plot_average_error(truth_votes_mapped, truth_ranking_mapped)
 
 
 if __name__ == '__main__':
